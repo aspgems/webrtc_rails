25e95c42546be70394ad41b17d2ceb7ff07918ef
Gabriel Ortuño <arctarusnet@gmail.com>
2014-03-11 16:19:07 +0100

    added share screen button

--- app/assets/javascripts/webrtc_rails/rooms.js
+++ app/assets/javascripts/webrtc_rails/rooms.js
@@ -5,14 +5,42 @@ var Room = {
   initialize: function(local, remotes){
     if ($('#'+local).size() <= 0 || $('#'+remotes).size() <= 0) { return false; }
 
+    var name = $('h1').html();
+
     var webrtc = new SimpleWebRTC({
       localVideoEl: local,
       remoteVideosEl: remotes,
       autoRequestMedia: true
     });
 
-    webrtc.on('readyToCall', function () {
-      webrtc.joinRoom($('h1').html());
+    webrtc.on('readyToCall', function() {
+      webrtc.joinRoom(name);
     });
+
+    webrtc.createRoom(name, function(err, name) {
+      if (err) { return false; }
+      setRoom(name);
+    });
+
+    var button = $('#screenShareButton');
+    var setButton = function(bool) {
+      button.text(bool ? 'share screen' : 'stop sharing');
+    };
+
+    setButton(true);
+
+    if (!webrtc.screenSharingSupport) {
+      button[0].disabled = true;
+    } else {
+      button.click(function() {
+        if (webrtc.localScreen) {
+          webrtc.stopScreenShare();
+          setButton(true);
+        } else {
+          webrtc.shareScreen();
+          setButton(false);
+        }
+      });
+    }
   }
 }
